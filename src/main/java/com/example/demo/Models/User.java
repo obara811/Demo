09e305185a60c4eb196d7d2082b3b139013fe427
hangoutyokki <sapporo.hangout@gmail.com>
2022-05-24 17:03:22 +0900

    mvc

---
+++ src/main/java/com/example/demo/Models/User.java
@@ -0,0 +1,11 @@
+package com.example.demo.Models;
+
+  import lombok.Getter;
+  import lombok.Setter;
+  @Getter
+  @Setter
+public class User {
+    private String name;
+    private int age;
+    private String email;
+}
